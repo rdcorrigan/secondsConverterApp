689a48a0d2b3cc1b69635de5acbb3670ab8b6ca6
Ryan Corrigan <ryandcorrigan@gmail.com>
2023-05-04 10:49:08 -0600

    *month equals 30.44 days

--- humanReadableDurationFormat.js
+++ humanReadableDurationFormat.js
@@ -1,58 +1,69 @@
-// MONTHS UNIT ADDED
+// Your task in order to complete this Kata is to write a function which formats a duration, given as a number of seconds, in a human-friendly way.
+
+// The function must accept a non-negative integer. If it is zero, it just returns "now". Otherwise, the duration is expressed as a combination of years, days, hours, minutes 
+// and seconds.
+
+// It is much easier to understand with an example:
+
+// formatDuration(62)    // returns "1 minute and 2 seconds"
+// formatDuration(3662)  // returns "1 hour, 1 minute and 2 seconds"
+// For the purpose of this Kata, a year is 365 days and a day is 24 hours.
+
+// Note that spaces are important.
+
+// Detailed rules
+// The resulting expression is made of components like 4 seconds, 1 year, etc. In general, a positive integer and one of the valid units of time, separated by a space. 
+// The unit of time is used in plural if the integer is greater than 1.
+
+// The components are separated by a comma and a space (", "). Except the last component, which is separated by " and ", just like it would be written in English.
+
+// A more significant units of time will occur before than a least significant one. Therefore, 1 second and 1 year is not correct, but 1 year and 1 second is.
+
+// Different components have different unit of times. So there is not repeated units like in 5 seconds and 1 second.
+
+// A component will not appear at all if its value happens to be zero. Hence, 1 minute and 0 seconds is not valid, but it should be just 1 minute.
+
+// A unit of time must be used "as much as possible". It means that the function should not return 61 seconds, but 1 minute and 1 second instead. Formally, the duration 
+// specified by of a component must not be greater than any valid more significant unit of time.
+
 
 const formatDuration = (seconds) => {
-  let sec, min, hour, day, month, year, time = seconds, arr = [], obj = {};
-  if (time == 0) return "now";
-  // store # of seconds in object
+  if (seconds === 0) return "now";
+  
+  let sec, min, hour, day, year, time = seconds, arr = [], obj = {};
+  
   if (time >= 60) {
-    console.log('seconds:', time);
     sec = time % 60;
     obj["sec"] = sec;
     min = Math.floor(time/60);
     obj["min"] = min;
     time = min;
-    if (min < 60) time = 0;
   } else {
     obj["sec"] = time;
   }
-  // store # of minutes in object
   if (time >= 60) {
     min = time % 60;
     obj["min"] = min;
     hour = Math.floor(time/60);
     obj["hour"] = hour;
     time = hour;
-    if (hour < 24) time = 0;
   }
-  // store # of hours in object
   if (time >= 24) {
     hour = time % 24;
     obj["hour"] = hour;
     day = Math.floor(time/24);
     obj["day"] = day;
     time = day;
-    if (day < 30.44) time = 0;
   }
-  // store # of days in object
-  if (time >= 30.44) {
-    day = Math.floor(time % 30.44);
+  if (time >= 365) {
+    day = time % 365;
     obj["day"] = day;
-    month = Math.floor(time/30.44);
-    obj["month"] = month;
-    time = month;
-    if (month < 12) time = 0;
-  }
-  // store # of months and years in object
-  if (time >= 12) {
-    month = time % 12;
-    obj["month"] = month;
-    year = Math.floor(time/12);
+    year = Math.floor(time/365);
     obj["year"] = year;
   }
-  // create string array with object values
-  // SECONDS
+  
   if (obj["sec"]) {
-    if (obj["min"] || obj["hour"] || obj["day"] || obj["month"] || obj["year"]) {
+    if (obj["min"] || obj["hour"] || obj["day"] || obj["year"]) {
       if (obj["sec"] === 1) {
         arr.unshift(" and 1 second")
       } else {
@@ -66,10 +77,9 @@ const formatDuration = (seconds) => {
         }
      }
     }
-  // MINUTES
   if (obj["min"]) {
-    if (obj["hour"] || obj["day"] || obj["month"] || obj["year"]) {
-      if (!arr.length) {
+    if (obj["hour"] || obj["day"] || obj["year"]) {
+      if (arr.length < 1) {
         if (obj["min"] === 1) {
           arr.unshift(" and 1 minute");
       } else {
@@ -90,10 +100,9 @@ const formatDuration = (seconds) => {
      }
    }
   }
-  // HOURS
   if (obj["hour"]) {
-    if (obj["day"] || obj["month"] || obj["year"]) {
-      if (!arr.length) {
+    if (obj["day"] || obj["year"]) {
+      if (arr.length < 1) {
         if (obj["hour"] === 1) {
           arr.unshift(" and 1 hour");
         } else {
@@ -114,10 +123,9 @@ const formatDuration = (seconds) => {
       }
     }
   }
-  // DAYS
   if (obj["day"]) {
-    if (obj["month"] || obj["year"]) {
-      if (!arr.length) {
+    if (obj["year"]) {
+      if (arr.length < 1) {
         if (obj["day"] === 1) {
           arr.unshift(" and 1 day");
         } else {
@@ -138,33 +146,8 @@ const formatDuration = (seconds) => {
       }
     }
   }
-  // MONTHS
-  if (obj["month"]) {
-    if (obj["year"]) {
-      if (!arr.length) {
-        if (obj["month"] === 1) {
-          arr.unshift(" and 1 month");
-        } else {
-          arr.unshift(" and " + month.toString() + " months");
-        }
-      } else {
-        if (obj["month"] === 1) {
-          arr.unshift(", 1 month");
-        } else {
-          arr.unshift(", " + month.toString() + " months");
-        }
-      }
-    } else {
-      if (obj["month"] === 1) {
-        arr.unshift("1 month");
-      } else {
-        arr.unshift(month.toString() + " months");
-      }
-    }
-  }
-  // YEARS
   if (obj["year"]) {
-    if (!arr.length) {
+    if (arr.length < 1) {
       if (obj["year"] === 1) {
         return "1 year";
       } else {
@@ -182,14 +165,12 @@ const formatDuration = (seconds) => {
 }
 
 
-// console.log(formatDuration(76554));
-
 const convert = () => {
   const value = document.querySelector('input').value, 
   appendDiv = document.createElement('div'), 
   div = document.querySelector('#appendToMe'), 
   output = formatDuration(value);
-  if (value.length) {
+  if (value.length > 0) {
     appendDiv.append(value + " seconds equals to " + output);
     appendDiv.classList.add('output')
     div.append(appendDiv);
@@ -197,7 +178,7 @@ const convert = () => {
 }
 
 const clear = () => {
-  let input = document.querySelector('input');
+  let input = document.querySelector('#input');
   input.value = "";
 }
 
@@ -205,13 +186,10 @@ const input = document.querySelector('#input'),
 convertButton = document.querySelector('#convert'), 
 clearButton = document.querySelector('#clear'), 
 resetAll = document.querySelector('#reset');
-
 input.addEventListener('keypress', (event) => {
   if (event.keyCode === 13) convert(); });
 convertButton.addEventListener('click', convert);
 clearButton.addEventListener('click', clear);
 resetAll.addEventListener('click', 
 window.location.reload.bind(window.location));
-
-
 
